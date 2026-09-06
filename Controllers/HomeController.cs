3264845944ad639f98d999cf39c9c0c73d344ac9
Sourav Kayal <sourav.kayal@yahoo.com>
2026-09-06 17:39:55 +0530

    aaa

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,6 +7,7 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         // This is the default action for the HomeController. It returns the Index view.
+        //new code
         return View();
     }
 }
