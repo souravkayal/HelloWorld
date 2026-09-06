6ac62fcdb043d4892938bbd284652a6a191d1737
Sourav Kayal <sourav.kayal@yahoo.com>
2026-09-06 17:20:07 +0530

    comment added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,8 +6,7 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        // This is the default action for the HomeController. It returns the Index view.
-        //new code
+        //This is comment added for testing.
         return View();
     }
 }
